e02f30d38ea763d6fe6ab92bcdf751bb28670f0b
YiranLuo <yiranluo1210@gmail.com>
2021-10-24 16:45:51 -0700

    Added validity checking for every input

--- src/warehouse/Main.java
+++ src/warehouse/Main.java
@@ -20,23 +20,27 @@ public class Main {
 
         /* Reading data from data file and setting graph
          */
-        try {
-            System.out.println("please input the DataFile's address");
-            String filePath = scanner.nextLine();
 
-            while(!filePath.endsWith("txt")) {
-                System.out.println("your DataFile is wrong, please input again:");
-                filePath = scanner.nextLine();
+        /*
+        Loop until successfully read the txt file
+         */
+        boolean readSuccessful = false;
+        while (!readSuccessful){
+            try {
+                System.out.println("Please input the file of the txt file:");
+                String filePath = scanner.nextLine();
+
+                primaryController.readAllItems(filePath);
+                readSuccessful = true;
             }
-
-            primaryController.readAllItems(filePath);
-        }
 //        try {
 //            primaryController.readAllItems("src/warehouse/qvBox-warehouse-data-f21-v01.txt");
 //        }
-        catch (IOException e){
-            System.out.println("invalid file exception");
+            catch (IOException e){
+                System.out.println("Invalid file path!");
+            }
         }
+
 
         primaryController.setGraph();
         System.out.println("All items from data file have been stored and set in the warehouse map");
@@ -58,37 +62,85 @@ public class Main {
                 - display location and path to get to item on map
             - otherwise exit program
          */
-        System.out.println("What would you like to do?");
-        System.out.println("1: find object");
-        System.out.println("2: exit");
-
-        int choice = scanner.nextInt();
-
-        while (choice != 2) {
-            System.out.println("Enter item id: ");
-            int itemID = scanner.nextInt();
-            // primaryController.markItemInGraph(itemID);
-            if (!primaryController.markItemInGraph(itemID)) {
-                System.out.println("The item you are looking for does NOT exist!");
-                continue;
-            }
-            System.out.println();
-
-            System.out.println("The item for id: " + itemID + " is marked as '$' on the map.");
-            System.out.println("The path from your location 'U' to the item '$' is marked with 'P' on the map.");
-            String shortestPathOutput = primaryController.findItemAndCallPath(itemID);
-            System.out.println(shortestPathOutput);
-            primaryController.markPathOnGraph();
-            primaryController.printGraph();
-            primaryController.unmarkPathOnGraph();
-            primaryController.unmarkItemInGraph(itemID);
-            System.out.println();
-
+        int choice;
+        boolean loopFlag = true;
+        while (loopFlag) {
             System.out.println("What would you like to do?");
             System.out.println("1: find object");
             System.out.println("2: exit");
-            choice = scanner.nextInt();
+            if (scanner.hasNextInt()) {
+                 choice = scanner.nextInt();
+            } else {
+                String str = scanner.next();
+                System.out.println("Invalid input! Please input '1' or '2'.");
+                continue;
+            }
+            switch (choice) {
+                case 1:
+                    System.out.println("Enter item id: ");
+                    if (scanner.hasNextInt()) {
+                        int itemID = scanner.nextInt();
+                        // primaryController.markItemInGraph(itemID);
+                        if (!primaryController.markItemInGraph(itemID)) {
+                            System.out.println("The item you are looking for does NOT exist!");
+                            continue;
+                        }
+                        System.out.println();
+
+                        System.out.println("The item for id: " + itemID + " is marked as '$' on the map.");
+                        System.out.println("The path from your location 'U' to the item '$' is marked with 'P' on the map.");
+                        String shortestPathOutput = primaryController.findItemAndCallPath(itemID);
+                        System.out.println(shortestPathOutput);
+                        primaryController.markPathOnGraph();
+                        primaryController.printGraph();
+                        primaryController.unmarkPathOnGraph();
+                        primaryController.unmarkItemInGraph(itemID);
+                        System.out.println();
+                    } else {
+                        String str = scanner.next();
+                        System.out.println("Invalid input! Please input a number.");
+                    }
+                    break;
+                case 2:
+                    loopFlag = false;
+                    System.out.println("Good Bye.");
+                    break;
+                default:
+                    System.out.println("Invalid input! Please input '1' or '2'.");
+            }
         }
+
+//        System.out.println("What would you like to do?");
+//        System.out.println("1: find object");
+//        System.out.println("2: exit");
+//
+//        int choice = scanner.nextInt();
+//
+//        while (choice != 2) {
+//            System.out.println("Enter item id: ");
+//            int itemID = scanner.nextInt();
+//            // primaryController.markItemInGraph(itemID);
+//            if (!primaryController.markItemInGraph(itemID)) {
+//                System.out.println("The item you are looking for does NOT exist!");
+//                continue;
+//            }
+//            System.out.println();
+//
+//            System.out.println("The item for id: " + itemID + " is marked as '$' on the map.");
+//            System.out.println("The path from your location 'U' to the item '$' is marked with 'P' on the map.");
+//            String shortestPathOutput = primaryController.findItemAndCallPath(itemID);
+//            System.out.println(shortestPathOutput);
+//            primaryController.markPathOnGraph();
+//            primaryController.printGraph();
+//            primaryController.unmarkPathOnGraph();
+//            primaryController.unmarkItemInGraph(itemID);
+//            System.out.println();
+//
+//            System.out.println("What would you like to do?");
+//            System.out.println("1: find object");
+//            System.out.println("2: exit");
+//            choice = scanner.nextInt();
+//        }
 
     }
 
